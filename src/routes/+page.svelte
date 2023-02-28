20c6b9b23b8f0b19993895be09644dfdad9778db
Cole Geerts <cjgeerts@gmail.com>
2023-02-27 23:07:35 -0500

    Init

---
+++ src/routes/+page.svelte
@@ -0,0 +1,14 @@
+<p class="text top-text">Cole Geerts</p>
+<p class="text">Great things are Coming</p>
+
+<style>
+  .top-text {
+    padding-top: 40vh;
+  }
+
+  .text {
+    width: 100%;
+    margin: 0 auto;
+    text-align: center;
+  }
+</style>
