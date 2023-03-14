bfd97828bdce29eab16e4d506606310499f89e6b
Cole Geerts <cjgeerts@gmail.com>
2023-03-14 15:40:42 -0400

    Swapping Fonts

--- svelte.config.js
+++ svelte.config.js
@@ -7,7 +7,6 @@ const config = {
 			// default options are shown. On some platforms
 			// these options are set automatically — see below
 			pages: 'build',
-			assets: 'build',
 			fallback: null,
 			precompress: false,
 			strict: true
