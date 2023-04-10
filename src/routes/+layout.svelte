8b23e1d6cb06c04c5695eed45408182457bc6b2a
Cole Geerts <cjgeerts@gmail.com>
2023-04-10 00:23:25 -0400

    Adding Blotter VHS Effect

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script>
   import "../app.css";
+  import BlotterEffects from "./BlotterEffects.svelte";
 </script>
 
+<BlotterEffects />
 <slot />
---
+++ src/routes/BlotterEffects.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+
+  const blotterInit = () => {
+    let text = new Blotter.Text("observation", {
+      family: "'EB Garamond', serif",
+      size: 48,
+      fill: "#171717",
+      // paddingLeft: 40,
+      // paddingRight: 40,
+    });
+
+    let material = new Blotter.ChannelSplitMaterial();
+
+    material.uniforms.uOffset.value = 0.05;
+    material.uniforms.uRotation.value = 240.0;
+    material.uniforms.uApplyBlur.value = 0.0;
+    material.uniforms.uAnimateNoise.value = 1.0;
+
+    let blotter = new Blotter(material, {
+      texts: text,
+    });
+
+    let elem = document.getElementById("blotter");
+    let scope = blotter.forText(text);
+
+    scope.appendTo(elem);
+  };
+
+  // onMount(blotterInit);
+</script>
+
+<div id="blotter" />
+
+<style>
+  #blotter {
+    position: absolute;
+  }
+</style>
