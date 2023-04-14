c201143b5c2c973fd49bb0b66029b8e41d254a1a
Cole Geerts <cjgeerts@gmail.com>
2023-04-14 13:09:38 -0400

    Adding Profile Pic and Git Link

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,16 +3,33 @@
 </script>
 
 <Contact />
-<p class="text top-text">Cole Geerts</p>
-<p class="text">Great things are Coming</p>
-<nav>
-  <a href="resume">r&eacute;sum&eacute;</a>
-  <a href="misc">misc</a>
-</nav>
+<div class="center">
+  <div class="pp-con">
+    <img src="pp.jpg" alt="Profile" />
+  </div>
+  <p class="text top-text">Cole Geerts</p>
+  <p class="text">Great things are Coming</p>
+  <nav>
+    <a href="resume">r&eacute;sum&eacute;</a>
+    <a
+      href="https://github.com/ColeyG"
+      target="_blank"
+      rel="noopener noreferrer">github</a
+    >
+    <a href="misc">misc</a>
+  </nav>
+</div>
 
 <style>
+  .center {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    height: 100%;
+  }
+
   .top-text {
-    padding-top: 40vh;
     z-index: 100;
     position: relative;
   }
@@ -23,6 +40,22 @@
     text-align: center;
     z-index: 100;
     position: relative;
+  }
+
+  .pp-con {
+    z-index: 100;
+    margin-bottom: 34px;
+    width: 120px;
+    height: 120px;
+    overflow: hidden;
+    border-radius: 50%;
+    filter: grayscale(1);
+  }
+
+  .pp-con img {
+    width: 100%;
+    height: 100%;
+    object-fit: cover;
   }
 
   nav {
