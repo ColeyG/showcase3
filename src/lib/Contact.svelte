874868c7bfdb3db95de9a4cbc65310ff18973220
Cole Geerts <cjgeerts@gmail.com>
2024-06-29 20:25:07 -0400

    Change Message Route

--- src/lib/Contact.svelte
+++ src/lib/Contact.svelte
@@ -36,7 +36,7 @@
       return;
     }
 
-    fetch("https://coley.world/api/custom-discord-message", {
+    fetch("http://99.242.124.248:8921/custom-discord-message", {
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
